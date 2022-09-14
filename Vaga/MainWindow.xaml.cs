db4a1dd37b0b38bbcc770f46fe48611c75d0cec9
Akagami no Shanks <89997083+MustafaAlibekov@users.noreply.github.com>
2022-09-14 12:23:30 +0300

    Ты кто и какого ъуя на меня подписался?

--- Vaga/MainWindow.xaml.cs
+++ Vaga/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 namespace Vaga
 {
     /// <summary>
+    /// Ты кто?
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
